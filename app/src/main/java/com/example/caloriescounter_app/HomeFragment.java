650b96716871ec02551fd25f994c705210ba65fd
AlexandraPauna <alexandrapauna927@yahoo.com>
2019-04-13 02:33:33 +0300

    wip

---
+++ app/src/main/java/com/example/caloriescounter_app/HomeFragment.java
@@ -0,0 +1,36 @@
+package com.example.caloriescounter_app;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
