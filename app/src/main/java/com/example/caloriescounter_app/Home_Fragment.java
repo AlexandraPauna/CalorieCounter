70b338825e95c1e385f40cbe46ae6da7b9aaae99
AlexandraPauna <alexandrapauna927@yahoo.com>
2019-04-23 22:53:41 +0300

    fixed

--- app/src/main/java/com/example/caloriescounter_app/Home_Fragment.java
+++ app/src/main/java/com/example/caloriescounter_app/Home_Fragment.java
@@ -1,19 +1,33 @@
 package com.example.caloriescounter_app;
 
 
+import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.SystemClock;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.text.DateFormat;
 import java.util.Date;
+
+import static android.content.Context.ALARM_SERVICE;
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 
 /**
@@ -22,6 +36,13 @@ import java.util.Date;
 public class Home_Fragment extends Fragment {
 
     OnActivityFragmentCommunication onActivityFragmentCommunication;
+
+    // Notification ID.
+    private static final int NOTIFICATION_ID = 0;
+    // Notification channel ID.
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+    private NotificationManager mNotificationManager;
 
     public Home_Fragment() {
         // Required empty public constructor
@@ -51,6 +72,64 @@ public class Home_Fragment extends Fragment {
             }
         });
 
+
+        mNotificationManager = (NotificationManager)
+                getContext().getSystemService(NOTIFICATION_SERVICE);
+
+        ToggleButton alarmToggle = view.findViewById(R.id.alarmToggle);
+        // Set up the Notification Broadcast Intent.
+        Intent notifyIntent = new Intent(getContext(), MyReceiver.class);
+
+        boolean alarmUp = (PendingIntent.getBroadcast(getContext(), NOTIFICATION_ID,
+                notifyIntent, PendingIntent.FLAG_NO_CREATE) != null);
+        alarmToggle.setChecked(alarmUp);
+
+        final PendingIntent notifyPendingIntent = PendingIntent.getBroadcast
+                (getContext(), NOTIFICATION_ID, notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final AlarmManager alarmManager = (AlarmManager) getContext().getSystemService
+                (ALARM_SERVICE);
+
+        // Set the click listener for the toggle button.
+        alarmToggle.setOnCheckedChangeListener
+                (new CompoundButton.OnCheckedChangeListener() {
+                    @Override
+                    public void onCheckedChanged
+                            (CompoundButton buttonView, boolean isChecked) {
+                        String toastMessage;
+                        if (isChecked) {
+
+                            long repeatInterval = 10*1000;
+
+                            long triggerTime = SystemClock.elapsedRealtime()
+                                    + repeatInterval;
+
+                            if (alarmManager != null) {
+                                alarmManager.setInexactRepeating
+                                        (AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                                                triggerTime, repeatInterval,
+                                                notifyPendingIntent);
+                            }
+                            toastMessage = "Alarm is on";
+
+                        } else {
+                            mNotificationManager.cancelAll();
+
+                            if (alarmManager != null) {
+                                alarmManager.cancel(notifyPendingIntent);
+                            }
+                            toastMessage = "Alarm is off";
+
+                        }
+
+                        Toast.makeText(getContext(), toastMessage,
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        createNotificationChannel();
+
         return view;
     }
 
@@ -59,6 +138,28 @@ public class Home_Fragment extends Fragment {
         super.onAttach(context);
         if(context instanceof OnActivityFragmentCommunication){
             onActivityFragmentCommunication = (OnActivityFragmentCommunication) context;
+        }
+    }
+
+    public void createNotificationChannel() {
+
+        mNotificationManager =
+                (NotificationManager) getContext().getSystemService(NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >=
+                android.os.Build.VERSION_CODES.O) {
+
+            NotificationChannel notificationChannel = new NotificationChannel
+                    (PRIMARY_CHANNEL_ID,
+                            "Stand up notification",
+                            NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to " +
+                    "stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
         }
     }
 
--- app/src/main/java/com/example/caloriescounter_app/MyReceiver.java
+++ app/src/main/java/com/example/caloriescounter_app/MyReceiver.java
@@ -10,10 +10,9 @@ import androidx.core.app.NotificationCompat;
 
 public class MyReceiver extends BroadcastReceiver {
 
-
     private NotificationManager mNotificationManager;
+    // Notification ID.
     private static final int NOTIFICATION_ID = 0;
-
     // Notification channel ID.
     private static final String PRIMARY_CHANNEL_ID =
             "primary_notification_channel";
@@ -22,23 +21,31 @@ public class MyReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         mNotificationManager = (NotificationManager)
                 context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Deliver the notification.
         deliverNotification(context);
     }
 
-
     private void deliverNotification(Context context) {
+        // Create the content intent for the notification, which launches
+        // this activity
         Intent contentIntent = new Intent(context, Main_Activity.class);
+
         PendingIntent contentPendingIntent = PendingIntent.getActivity
-                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
-                //.setSmallIcon(R.drawable.ic_stand_up)
-                .setContentTitle("Stand Up Alert")
-                .setContentText("You should stand up and walk around now!")
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent
+                        .FLAG_UPDATE_CURRENT);
+        // Build the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder
+                (context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_assignment_late_black_24dp)
+                .setContentTitle("Calories counter")
+                .setContentText("Stand up")
                 .setContentIntent(contentPendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
                 .setAutoCancel(true)
                 .setDefaults(NotificationCompat.DEFAULT_ALL);
-        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
 
+        // Deliver the notification
+        mNotificationManager.notify(NOTIFICATION_ID, builder.build());
     }
 }
