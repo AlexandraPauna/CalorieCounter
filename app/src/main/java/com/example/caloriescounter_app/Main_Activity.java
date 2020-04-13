2ed13113ecf111c304a3760e57324dacd05b4987
Andreea <andrex_97@yahoo.com>
2020-04-13 21:23:52 +0300

    Navigation Drawer butoane

--- app/src/main/java/com/example/caloriescounter_app/Main_Activity.java
+++ app/src/main/java/com/example/caloriescounter_app/Main_Activity.java
@@ -1,8 +1,12 @@
 package com.example.caloriescounter_app;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NotificationCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -23,25 +27,47 @@ import android.widget.CompoundButton;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.google.android.material.navigation.NavigationView;
 
 
-public class Main_Activity extends AppCompatActivity implements OnActivityFragmentCommunication {
+public class Main_Activity extends AppCompatActivity implements OnActivityFragmentCommunication, NavigationView.OnNavigationItemSelectedListener {
 
     private static final String FRAGMENT_TAG = "FRAGMENT_TAG";
+    private DrawerLayout drawer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+//        if (savedInstanceState == null) {
+//            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+//                    new MessageFragment()).commit();
+//            navigationView.setCheckedItem(R.id.nav_message);
+//        }
+
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.drawer_layout,new Home_Fragment(),FRAGMENT_TAG);
         fragmentTransaction.setTransition(fragmentTransaction.TRANSIT_FRAGMENT_FADE);
         String backStateName = Home_Fragment.class.getName();
         fragmentTransaction.addToBackStack(backStateName);
         fragmentTransaction.commit();
+
 
     }
 
@@ -103,7 +129,29 @@ public class Main_Activity extends AppCompatActivity implements OnActivityFragme
     }
 
     @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_meal:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Meals_Fragment()).commit();
+                break;
+            case R.id.nav_activity:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Activities_Fragment()).commit();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
     public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
         if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
 
             Fragment fragment = getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
