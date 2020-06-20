fcfee3872990d33ebb37f23aca9ee1cd677f1639
AlexandraPauna <alexandrapauna927@yahoo.com>
2020-06-20 19:56:24 +0300

    Share method implemented - share user photo

--- app/src/main/java/com/example/caloriescounter_app/Profile_Fragment.java
+++ app/src/main/java/com/example/caloriescounter_app/Profile_Fragment.java
@@ -1,6 +1,7 @@
 package com.example.caloriescounter_app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -8,13 +9,16 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.text.DecimalFormat;
 
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 public class Profile_Fragment extends Fragment {
@@ -60,24 +64,45 @@ public class Profile_Fragment extends Fragment {
         });
 
         Button SharePhotoBtn = (Button) view.findViewById(R.id.btn_share);
-        SharePhotoBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //TO DO
-
-            }
-        });
 
         final ImageView displayUserPhoto = (ImageView) view.findViewById(R.id.iv_user_image);
-        String imagePath = (String)new UserPhotoRepository(getContext()).getImage(userId);
+        final String imagePath = (String)new UserPhotoRepository(getContext()).getImage(userId);
         if(imagePath != null){
             displayUserPhoto.setImageURI(Uri.fromFile(new File(imagePath)));
         }
-        else{ //daca nu exista nicio poza salvata se ia din drwable
+        else{ //daca nu exista nicio poza salvata se ia din drawable
             System.out.println("User does not have profile photo!");
             Drawable myDrawable = getResources().getDrawable(R.drawable.no_profile);
             displayUserPhoto.setImageDrawable(myDrawable);
+
+            //se ascunde butonul de share poza daca nu exista poza stocata
+            SharePhotoBtn.setVisibility(View.INVISIBLE);
         }
+
+        SharePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try
+                {
+                    File myFile = new File(imagePath);
+                    MimeTypeMap mime = MimeTypeMap.getSingleton();
+                    String ext = myFile.getName().substring(myFile.getName().lastIndexOf(".") + 1);
+                    String type = mime.getMimeTypeFromExtension(ext);
+                    Intent sharingIntent = new Intent("android.intent.action.SEND");
+                    sharingIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    sharingIntent.setType(type);
+                    Uri photoURI = FileProvider.getUriForFile(getContext(),
+                            getContext().getApplicationContext().getPackageName() + ".provider", myFile);
+                    sharingIntent.putExtra("android.intent.extra.STREAM", photoURI);
+                    startActivity(Intent.createChooser(sharingIntent, "Share using"));
+                }
+                catch (Exception e)
+                {
+                    Toast.makeText(getContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 //        double weight = (double)new UserRepository(getContext()).getWeight(userId);
 //        double height = (double)new UserRepository(getContext()).getHeight(userId)/100;
@@ -86,8 +111,6 @@ public class Profile_Fragment extends Fragment {
 //        double bmiResult = Double.valueOf(twoDForm.format(bmi));
 //        final TextView displayBmi = (TextView) view.findViewById(R.id.tv_bmi);
 //        displayBmi.setText("BMI: " + String.valueOf(bmiResult));
-
-
 
         return view;
     }
